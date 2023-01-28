801b9f214bd0f8991afeaae4752a61653cf1b2df
GUchechi <godswilluchendu7@gmail.com>
2023-01-28 23:52:07 +0100

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo2 from "../Assets/logo2.png";
 import {
+  FaGift,
   FaInstagram,
   FaTelegramPlane,
   FaTwitter,
@@ -11,7 +12,7 @@ const Footer = () => {
     <div className="">
       <div className=" w-full flex items-center justify-center h-full bg-[#920F0F] text-white py-4  gap-[30px]">
         <div className="">
-          <img src={logo2} width="55px" height="55px" className="mx-3" alt="" />
+          <FaGift size={22} height="55px" className="mx-3 animate-spin"  alt="" />
         </div>
         <div className="font-bold mr-1 flex gap-6">
           <FaTwitter size={25} />
@@ -19,7 +20,7 @@ const Footer = () => {
           <FaTelegramPlane size={25} />
         </div>
         <div className="font-bold text-[16px] mr-3">
-          <p>&copy; ifẹ, 2023</p>
+          <p>&copy;2023</p>
         </div>
       </div>
     </div>
