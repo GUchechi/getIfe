3b5d7d6b7012fc1f6b3f6b2f57517b3a9e8c1b17
GUchechi <godswilluchendu7@gmail.com>
2023-01-28 23:29:04 +0100

    added typewrite effect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import hero from "../Assets/Hero.jpg";
 import { FaInstagram, FaTelegramPlane, FaTwitter } from "react-icons/fa";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
@@ -10,7 +11,17 @@ const Home = () => {
         <div className="absolute w-full h-full text-white bg-black/60 flex flex-col">
           <div className="flex flex-col top-1 gap-4 lg:gap-[40px] absolute lg:top-[28%] px-7">
             <h1 className="px-4 text-2xl md:text-5xl lg:text-6xl font-bold">
-              World's largest AI gift sharing platform
+              <Typewriter
+                options={{
+                  autoStart: true,
+                  loop: true,
+                  delay: 40,
+                  strings: [
+                    "World's largest AI gift sharing platform",
+                    "Send Gifts to Families, Friends, Clients, and Customers anywhere in the world",
+                  ],
+                }}
+              />
             </h1>
             <h5 className="text-1xl lg:text-4xl sm:text-2xl px-4">
               Share gifts easily and securely using blockchain technology
@@ -18,7 +29,8 @@ const Home = () => {
             <button className="py-2 px-4 w-44">Get Early Access</button>
             <div className="flex gap-5 lg:text-3xl text-1xl">
               <p>Follow us on</p>
-              <FaTwitter className="cursor-pointer" size={20} /> <FaTelegramPlane className="cursor-pointer" size={20} />{" "}
+              <FaTwitter className="cursor-pointer" size={20} />{" "}
+              <FaTelegramPlane className="cursor-pointer" size={20} />{" "}
               <FaInstagram className="cursor-pointer" size={20} />
             </div>
           </div>
