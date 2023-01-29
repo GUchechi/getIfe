c3b053e4ba12bd671246d6936d7825c135159efa
GUchechi <godswilluchendu7@gmail.com>
2023-01-29 13:27:28 +0100

    product section

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -5,7 +5,7 @@ import Rectangle67 from "../Assets/ss6.png";
 
 const ProductSection = () => {
   return (
-    <section className="bg-[#920F0F] text-white lg:py-[3rem] py-0 px-1">
+    <section className="bg-[#920F0F] text-white lg:py-[2rem] py-3 px-1">
       <div className="max-w-[1640px] mx-auto p-4 py-5 grid md:grid-cols-3 px-12 gap-6 ">
         <div className="rounded-xl relative">
           <img
@@ -32,7 +32,7 @@ const ProductSection = () => {
         </div>
       </div>
 
-      <p className="px-6 text-xl lg:text-2xl text-center">
+      <p className="px-6 py-4 text-[15px] lg:text-2xl text-center">
         ifẹ uses artificial intelligence to analyse the interests, lifestyle and
         preferences of both the giver and the recipients so that you can be sure
         that you are giving and receiving the perfect gift.
