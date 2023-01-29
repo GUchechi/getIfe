ca0fc3bf02f839ce427329fd94131e39bf366f0d
GUchechi <godswilluchendu7@gmail.com>
2023-01-29 12:38:16 +0100

    email validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
       )
       .then(
         (result) => {
-          alert("Thank you for signing up for our Waitlist!!!");
+          alert("Thank you, We will get back to you shortly");
           window.location.reload(false);
         },
         (error) => {
@@ -43,7 +43,7 @@ const Contact = () => {
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/3">
             <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-              Full Name
+              Name
             </label>
           </div>
           <div className="md:w-2/3">
@@ -51,7 +51,7 @@ const Contact = () => {
               className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-black-700 leading-tight focus:outline-none focus:bg-white"
               id="inline-full-name"
               type="text"
-              placeholder="Full Name"
+              placeholder="Name"
               name="user_name"
             />
           </div>
