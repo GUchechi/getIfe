e82b4ebc3568f4f87ee2022a7418d4dcaf6e542e
GUchechi <godswilluchendu7@gmail.com>
2023-01-29 17:37:18 +0100

    contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,91 +1,22 @@
-import React, { useRef } from "react";
-import emailjs from "@emailjs/browser";
+import React from "react";
+import Iframe from "./Iframe";
 
 const Contact = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        "service_489hglo",
-        "template_cekz6ec",
-        form.current,
-        "n-7vRxXnjRzFDNYcL"
-      )
-      .then(
-        (result) => {
-          alert("Thank you, We will get back to you shortly");
-          window.location.reload(false);
-        },
-        (error) => {
-          alert("Failed to Send Message, please try again.");
-        }
-      );
-    e.target.reset();
-  };
 
   return (
     <div
       name="contact"
-      className="flex flex-col bg-[#FEEFE5] justify-center items-center w-full h-full py-12"
+      className="flex flex-col bg-[#FEEFE5] justify-center items-center w-full h-full"
     >
-      <div className="flex flex-col justify-center items-center px-6 py-12">
+      <div className="flex flex-col justify-center items-center px-6 py-10 lg:py-12">
         <h2 className="font-bold text-center lg:text-6xl sm:text-4xl text-[48px]">
           Be among the first to experience ifẹ
         </h2>
-        <p className="text-center text-[20px] mt-4">
+        <p className="text-center lg:text-3xl text-[20px] mt-4">
           We’ll keep you updated when the product launches
         </p>
       </div>
-      <form ref={form} onSubmit={sendEmail} className="w-full px-6 max-w-md">
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-              Name
-            </label> 
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className=" appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-black-700 leading-tight focus:outline-none focus:bg-white"
-              id="inline-full-name"
-              type="text"
-              placeholder="Name"
-              name="user_name"
-              required
-            />
-          </div>
-        </div>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-              Email
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className=" appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white"
-              id="inline-password"
-              type="Email"
-              placeholder="Email"
-              required
-              name="user_email"
-            />
-          </div>
-        </div>
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3 flex items-center justify-center">
-            <button
-              className="items-center text-center justify-center shadow-xl bg-[#920F0F] hover:bg[#920F0F] focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded-full"
-              type="submit"
-            >
-              Sign Up
-            </button>
-          </div>
-        </div>
-      </form>
+      <Iframe/>
     </div>
   );
 };
---
+++ src/components/Iframe.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+const Iframe = () => {
+  return (
+    <div>
+      <div className="sib-form" style={{ textAlign: "center" }}>
+        <div id="sib-form-container" className="sib-form-container">
+          <div
+            id="sib-container"
+            className="sib-container--large sib-container--vertical iframe-first-container"
+          >
+            <form
+              id="sib-form"
+              method="POST"
+              action="https://7655f62d.sibforms.com/serve/MUIEAIyJeSo63v0nh6cFiZY7MWvCO7OXB67kX0mRhm9nsogxnJHJTBiv0-YbwNXHa1BWZjxkDrcSni21uFdF_8f8vek2DMb7fyAKpnFosQDwbDuqyIhEAlV2BSt_hbG-SA8O1fqp3soUkFDT9bP9B6tDKcwO5YvQDgXPhFKWWAhr2sPJhiQbYhMVE047bqtuGVP_eeI1SSOyQCmr"
+            >
+              <div style={{ padding: "8px 0" }}>
+                <div className="sib-input sib-form-block">
+                  <div className="form__entry entry_block">
+                    <div className="form__label-row">
+                      <div className="entry__field">
+                        <input
+                          className="input"
+                          maxLength="200"
+                          type="text"
+                          id="FIRSTNAME"
+                          name="FIRSTNAME"
+                          autoComplete="off"
+                          placeholder="Name"
+                          data-required="true"
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    <label className="entry__error entry__error--primary iframe-second"></label>
+                    <label className="entry__specification iframe-three">
+                      Enter Firstname
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div style={{ padding: "8px 0" }}>
+                <div className="sib-input sib-form-block">
+                  <div className="form__entry entry_block">
+                    <div className="form__label-row">
+                      <div className="entry__field">
+                        <input
+                          className="input"
+                          type="text"
+                          id="EMAIL"
+                          name="EMAIL"
+                          autoComplete="off"
+                          placeholder="EMAIL"
+                          data-required="true"
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    <label className="entry__error entry__error--primary iframe-four"></label>
+                    <label className="entry__specification iframe-five">
+                      Provide your email address to subscribe. For e.g
+                      abc@xyz.com
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div style={{ padding: "8px 0" }}>
+                <div className="sib-form-block" style={{ textAlign: "left" }}>
+                  <button
+                    className="sib-form-block__button sib-form-block__button-with-loader iframe-six"
+                    form="sib-form"
+                    type="submit"
+                  >
+                    <svg
+                      className="icon clickable__icon progress-indicator__icon sib-hide-loader-icon"
+                      viewBox="0 0 512 512"
+                    >
+                      <path d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z" />
+                    </svg>
+                    Submit
+                  </button>
+                </div>
+              </div>
+              <input
+                type="text"
+                name="email_address_check"
+                value=""
+                className="input--hidden"
+              />
+              <input type="hidden" name="locale" value="en" />
+              <input type="hidden" name="html_type" value="simple" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Iframe;
